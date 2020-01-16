77d225fd97064d6c3d4c80372f1e17142919f903
tznind <t.z.nind@dundee.ac.uk>
2020-01-16 18:30:13 +0000

    Added publish

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+[assembly: AssemblyProduct("Wanderer")]
+[assembly: AssemblyCopyright("Copyright (c) 2018 - 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// These should be replaced with correct values by the release process
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyInformationalVersion("0.0.1")]
