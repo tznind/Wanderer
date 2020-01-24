d3805bafa99fdb131951de998837f071bf08fe45
tznind <t.z.nind@dundee.ac.uk>
2020-01-24 21:44:35 +0000

    Added adjectives back into rooms

--- src/Factories/RoomFactory.cs
+++ src/Factories/RoomFactory.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using StarshipWanderer.Actions;
+﻿using System.Linq;
 using StarshipWanderer.Extensions;
 using StarshipWanderer.Factories.Blueprints;
-using StarshipWanderer.Items;
 using StarshipWanderer.Places;
-using StarshipWanderer.Relationships;
 
 namespace StarshipWanderer.Factories
 {
@@ -36,7 +30,10 @@ namespace StarshipWanderer.Factories
                 : world.Factions.GetRandomFaction(world.R);
 
             var room = new Room(blueprint.Name, world, blueprint.Tile) {ControllingFaction = faction};
-            
+
+            var availableAdjectives = AdjectiveFactory.GetAvailableAdjectives(room).ToArray();
+            room.Adjectives.Add(availableAdjectives[world.R.Next(0, availableAdjectives.Length)]);
+
             if (faction != null)
             {
                 //create some random NPCs
@@ -54,24 +51,5 @@ namespace StarshipWanderer.Factories
 
             return room;
         }
-        /*
-
-            var room = _buildersList[world.R.Next(_buildersList.Count)](world);
-
-            //give the room a random adjective
-            var availableAdjectives = AdjectiveFactory.GetAvailableAdjectives(room).ToArray();
-            room.Adjectives.Add(availableAdjectives[world.R.Next(0, availableAdjectives.Length)]);
-
-            if (world.Factions.Any())
-            {
-                var faction = room.ControllingFaction ?? world.Factions.GetRandomFaction(world.R);
-                faction.ActorFactory?.Create(world, room,faction);
-
-                var itemFactory = faction.ActorFactory?.ItemFactory;
-                if(itemFactory!=null && itemFactory.Blueprints.Any())
-                    room.Items.Add(itemFactory.Create(itemFactory.Blueprints.GetRandom(world.R)));
-            }
-            
-            return room;*/
     }
 }
--- src/Resources/README.md
+++ src/Resources/README.md
@@ -5,7 +5,9 @@ This file describes the relationship between classes and resource files
 |   Yaml File      |  Class           |   Notes                   |
 |------------------|------------------|---------------------------|
 | /Dialogue/*.yaml | `DialogueNode[]` | Contains unique dialogue trees.  Each node consists of 1 main piece of text and 0 or more options which can lead to other nodes or end dialogue |
+| /Rooms.yaml | `RoomBlueprint[]` | Contains descriptions of rooms that can be generated |
 | /Factions/X/Faction.yaml | `Faction` | Contains description of the faction (name, role etc) |
-| /Factions/X/Actors.yaml  | `ActorBlueprint` | Describes how to create `Actor` instances that fit thematically with the `Faction` whose folder they are in |
+| /Factions/X/Actors.yaml  | `ActorBlueprint[]` | Describes how to create `Actor` instances that fit thematically with the `Faction` whose folder they are in |
+| /Factions/X/Items.yaml  | `ItemBlueprint[]` | Describes how to create `Item` instances that fit thematically with the `Faction` whose folder they are in |
 | /Factions/X/Forenames.txt | `NameFactory` | If present then unamed Npc generated in this faction have random selections from this list |
 | /Factions/X/Surnames.txt | `NameFactory` | As above but for surnames |
