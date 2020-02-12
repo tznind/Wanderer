a428712298ec39aa48eaa0bdab519b6af1c9ba2e
tznind <tznind@dundee.ac.uk>
2020-02-12 15:30:27 +0000

    Added --validate CLI option

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,21 +1,41 @@
-﻿using Game.UI;
+﻿using System;
+using System.Linq;
+using Game.UI;
 using StarshipWanderer;
 using StarshipWanderer.Factories;
+using StarshipWanderer.Validation;
 using Terminal.Gui;
 
 namespace Game
 {
     class Program
     {
-
-
         static void Main(string[] args)
         {
             Application.Init();
-            
-            var mainWindow = new MainWindow(new WorldFactory());
-            Application.Top.Add(mainWindow);
-            Application.Run();
+
+            var f = new WorldFactory();
+
+            if(args.Any(a=>string.Equals(a,"--validate",StringComparison.CurrentCultureIgnoreCase)))
+            {
+                var validator = new WorldValidator();
+                validator.IncludeStackTraces = false;
+                validator.Validate(f);
+                
+                Console.WriteLine(validator.Warnings);
+
+                if(validator.Errors.Length > 0)
+                    Console.WriteLine(validator.Errors);
+                else
+                    Console.WriteLine("Validation Passed");
+            }
+            else
+            {
+                var mainWindow = new MainWindow(f);
+                Application.Top.Add(mainWindow);
+                Application.Run();                
+            }
+
         }
     }
 }
