a70e6187cdc2091ec2546e7a1018f128b2225191
tznind <t.z.nind@dundee.ac.uk>
2020-02-21 12:48:52 +0000

    Added more tests to understand lua

--- Tests/CompilerTests/TestLua.cs
+++ Tests/CompilerTests/TestLua.cs
@@ -38,13 +38,91 @@ import ('Wanderer','Wanderer.Stats')
 "
                     );
 
-                lua.DoString("result = you.GetFinalStats(you)[Stat.Corruption] > 50");
+                lua.DoString("result = you:GetFinalStats(you)[Stat.Corruption] > 50");
                 Assert.IsFalse((bool)lua["result"]);
 
                 you.BaseStats[Stat.Corruption] = 6000;
                 
-                lua.DoString("result = you.GetFinalStats(you)[Stat.Corruption] > 50");
+                lua.DoString("result = you:GetFinalStats(you)[Stat.Corruption] > 50");
                 Assert.IsTrue((bool)lua["result"]);
+            }
+        }
+
+
+        public class A
+        {
+            public string SayHi()
+            {
+                return "hi";
+            }
+            public string SayHiToStr(string tome)
+            {
+                return "hi " + tome;
+            }
+            public string SayHiTo(B b)
+            {
+                return "hi B";
+            }
+        }
+
+        public class B
+        {
+
+        }
+        
+        [Test]
+        public void TestLua_MethodCall()
+        {
+            using (Lua lua = new Lua())
+            {
+                var a = new A();
+                var b = new B();
+                lua["a"] = a;
+                lua["b"] = b;
+                
+                lua.LoadCLRPackage();
+                lua.DoString(
+                    @$"
+import ('System', 'System.String')
+import ('{typeof(A).Assembly.GetName().Name}', '{typeof(A).Namespace}')
+");
+                lua.DoString("result = a:SayHi()");
+                Assert.AreEqual("hi", lua["result"]);
+
+                lua.DoString("result = a:SayHiToStr('Goblin')");
+                Assert.AreEqual("hi Goblin", lua["result"]);
+
+                lua.DoString("result = a:SayHiTo(b)");
+                Assert.AreEqual("hi B", lua["result"]);
+            }
+        }
+
+        [Test]
+        public void TestLua_SubProperties()
+        {
+            
+            var you = YouInARoom(out IWorld world);
+
+            using (Lua lua = new Lua())
+            {
+                lua["you"] = you;
+                lua["location"] = you.CurrentLocation;
+
+                lua.LoadCLRPackage();
+                lua.DoString(@"
+import ('Wanderer', 'Wanderer')
+import ('Wanderer','Wanderer.Stats')
+import ('Wanderer','Wanderer.Places')
+"
+                );
+
+                lua.DoString("result = location:GetFinalStats(you)[Stat.Corruption] > 50");
+                Assert.IsFalse((bool) lua["result"]);
+
+                you.CurrentLocation.BaseStats[Stat.Corruption] = 6000;
+
+                lua.DoString("result = you.CurrentLocation:GetFinalStats(you)[Stat.Corruption] > 50");
+                Assert.IsTrue((bool) lua["result"]);
             }
         }
     }
