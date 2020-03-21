2c7e29a1ba754377dba493af191be181380d7443
Thomas Nind <t.z.nind@dundee.ac.uk>
2020-03-21 14:24:18 +0000

    Added Transition property to DialogueOption

--- src/Dialogues/DialogueOption.cs
+++ src/Dialogues/DialogueOption.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Wanderer.Compilation;
 using Wanderer.Systems;
 
@@ -22,12 +23,33 @@ namespace Wanderer.Dialogues
         /// </summary>
         public bool SingleUse { get; set; } = false;
 
+        internal bool AllConditionsMet(IWorld world, SystemArgs args)
+        {
+            if(Transition.HasValue)
+            {
+                var match  = args.Room.Get(Transition.Value).FirstOrDefault();
+    
+                //Cannot transition to an abscent npc or one who has nothing to say
+                if(match?.Dialogue?.Next == null)
+                    return false;
+            }
+
+            return Condition.All(c=>c.IsMet(args.World,args));
+        }
+
         /// <summary>
         /// Set to true to indicate that the option shouldn't be offered again
         /// (e.g. for <see cref="SingleUse"/> options)
         /// </summary>
         public bool Exhausted { get; set; } = false;
+        
 
+        /// <summary>
+        /// Makes dialogue option transition from talking to one room / actor to 
+        /// another (who must be present).  This combines a condition of the guid being
+        /// present and the state update if the option is picked
+        /// </summary>
+        public Guid? Transition { get; set; }
 
         public override string ToString()
         {
