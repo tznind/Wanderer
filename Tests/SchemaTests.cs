6e46282af61c77bd04510fd25be1b51d19f94bab
tznind <t.z.nind@dundee.ac.uk>
2020-03-30 10:06:10 +0100

    Initial look into autocomplete / existing tools

---
+++ Tests/SchemaTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NJsonSchema.Generation;
+using Wanderer.Dialogues;
+
+namespace Tests
+{
+    class SchemaTests
+    {
+        [Test]
+        public void Test()
+        {
+            
+            JsonSchemaGenerator generator = new JsonSchemaGenerator(new JsonSchemaGeneratorSettings());
+            var schema = generator.Generate(typeof(List<DialogueNode>));
+            
+        }
+    }
+}
