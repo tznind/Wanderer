fa296fc89fba4217ba449c363c75715bd5872581
Thomas Nind <t.z.nind@dundee.ac.uk>
2020-03-30 10:36:25 +0100

    added schema for injury systems

--- Tests/SchemaTests.cs
+++ Tests/SchemaTests.cs
@@ -4,18 +4,26 @@ using System.Text;
 using NUnit.Framework;
 using NJsonSchema.Generation;
 using Wanderer.Dialogues;
+using Wanderer.Systems;
 
 namespace Tests
 {
     class SchemaTests
     {
         [Test]
-        public void Test()
+        public void DialogueSchema()
         {
-            
             JsonSchemaGenerator generator = new JsonSchemaGenerator(new JsonSchemaGeneratorSettings());
             var schema = generator.Generate(typeof(List<DialogueNode>));
-            
+            TestContext.Out.WriteLine(schema.ToJson());
+        }
+
+        [Test]
+        public void InjurySystemSchema()
+        {
+            JsonSchemaGenerator generator = new JsonSchemaGenerator(new JsonSchemaGeneratorSettings());
+            var schema = generator.Generate(typeof(InjurySystem));
+            TestContext.Out.WriteLine(schema.ToJson());
         }
     }
 }
