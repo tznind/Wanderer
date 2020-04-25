3c1622de6edd0826f8d277f81ec50db4cea2b361
tznind <t.z.nind@dundee.ac.uk>
2020-04-25 08:02:32 +0100

    Try again

--- docs-build/docs/Program.cs
+++ docs-build/docs/Program.cs
@@ -1,14 +1,12 @@
-using System;
+﻿using System;
 using System.Net.Http;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
-namespace docs
+namespace BlazorPages.Client
 {
     public class Program
     {
@@ -17,7 +15,7 @@ namespace docs
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
