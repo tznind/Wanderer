3f93e6e9809e9b7450412add27b725bf7eebb8a7
tznind <t.z.nind@dundee.ac.uk>
2020-05-21 18:35:22 +0100

    Bump to 0.1.0

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 
 // These should be replaced with correct values by the release process
-[assembly: AssemblyVersion("0.0.18")]
-[assembly: AssemblyFileVersion("0.0.18")]
-[assembly: AssemblyInformationalVersion("0.0.18")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
